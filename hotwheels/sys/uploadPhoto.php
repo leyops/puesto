451f26ca74dd67a6b7dd81e7f576b5588c94539e
Leonardo Pineda <lpineda.romero@gmail.com>
2023-12-01 09:05:08 -0600

    Buscando subir archivos

--- hotwheels/sys/uploadPhoto.php
+++ hotwheels/sys/uploadPhoto.php
@@ -11,12 +11,14 @@ require("../../api.puesto/common/Func-Utils.php");
     //echo "entro post archivo";
     //Recogemos el archivo enviado por el formulario
     $archivo = $_FILES['archivo']['name'];
+    //$originalfilename = 
     //Si el archivo contiene algo y es diferente de vacio
     if (isset($archivo) && $archivo != "") {
        //Obtenemos algunos datos necesarios sobre el archivo
        $tipo = $_FILES['archivo']['type'];
        $tamano = $_FILES['archivo']['size'];
        $temp = $_FILES['archivo']['tmp_name'];
+       //echo "temp file: " . $temp;
        //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
       if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 100000000))) {
          echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>';
@@ -31,10 +33,12 @@ require("../../api.puesto/common/Func-Utils.php");
             $newFile = '../photos/'.$cont.'_'.$_POST['toy'].'_'.$archivo;
             $cont++;
          }
+
          $archivo = $newFile;
          //Si la imagen es correcta en tamaño y tipo
          //Se intenta subir al servidor
-         if (move_uploaded_file($temp, $archivo)) {
+         //if (move_uploaded_file($temp, $archivo)) {
+         if (move_uploaded_file($temp, "/var/www/html/puesto/hotwheels/photos/".$_FILES['archivo']['name'])) {
              //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
              chmod($archivo, 0777);
              //Mostramos el mensaje de que se ha subido co éxito
@@ -50,7 +54,7 @@ require("../../api.puesto/common/Func-Utils.php");
              
              
              $sql = "INSERT INTO ".$databaseName.".hotwheels_photos (consecutive_hotwheels,src) " .
-                     "VALUES ((SELECT consecutive FROM ".$databaseName.".hotwheelslist WHERE toy = '".$toyHotwheels."'),'".substr($archivo,3)."')" ;
+                     "VALUES ((SELECT consecutive FROM ".$databaseName.".hotwheelslist WHERE toy = '".$toyHotwheels."'),'".$_FILES['archivo']['name']."')" ;
 
              $resultado = mysqli_query($conexionMySQL, $sql);
 
